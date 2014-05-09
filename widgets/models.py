7de7e8777b532b1f46f1233ded6e8c03cb9022df
Daryl Antony <daryl@commoncode.com.au>
2014-05-09 11:06:14 +1000

    anti migrations

--- widgets/models.py
+++ widgets/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 from cqrs.models import CQRSModel, CQRSPolymorphicModel
+from entropy.base import (
+    EnabledMixin, SlugMixin, TitleMixin
+)
 
 
 class Widget(CQRSModel, EnabledMixin, SlugMixin, TitleMixin):
@@ -18,7 +21,7 @@ class Widget(CQRSModel, EnabledMixin, SlugMixin, TitleMixin):
 
     # @@@ provide a list of template choices or create a
     # WidgetTemplate class
-    template_name = models.Charfield(
+    template_name = models.CharField(
         max_length=512)
 
 
@@ -36,7 +39,7 @@ class WidgetMailChimpSignup(WidgetAspect):
     '''
     For example, create a widget that sign's up to Mailchimp
     '''
-    list_name = models.Charfield(
+    list_name = models.CharField(
         max_length=1024)
 
 
