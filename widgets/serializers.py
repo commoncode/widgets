fcd695f1d0a6b4ee1e2c660a210faec43dfc34af
Y3K <dboy.y3k@gmail.com>
2014-07-03 23:50:44 -0500

    Attributes

--- widgets/serializers.py
+++ widgets/serializers.py
@@ -1,4 +1,5 @@
 from cqrs.serializers import CQRSSerializer, CQRSPolymorphicSerializer
+from entropy.serializers import AttributeSerializer
 from images.serializers import ImageInstanceSerializer
 from menus.serializers import LinkSerializer
 
@@ -20,6 +21,7 @@ class WidgetAspectSerializer(CQRSPolymorphicSerializer):
 class WidgetSerializer(CQRSSerializer):
     image = ImageInstanceSerializer()
     template = WidgetTemplateSerializer()
+    attributes = AttributeSerializer(many=True)
     aspects = WidgetAspectSerializer(many=True)
 
     class Meta:
